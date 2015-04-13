14cb2f0619e0ea88a5c7aac13556b22e1c14e278
Johannes Maria Frank <jmfrank63@gmail.com>
2015-04-13 01:25:25 +0100

    made osx work and added gitignore

--- tabreak.py
+++ tabreak.py
@@ -4,15 +4,17 @@ of time.
 '''
 import webbrowser
 import time
+import sys
+import subprocess
 
 # our constant variables
 number_of_breaks = 0
 max_breaks = 3
 intervall = 10
-url = 'www.youtube.com/watch?v=pIgZ7gMze7A'
+url = 'http://www.youtube.com/watch?v=pIgZ7gMze7A'
 
 # loop until number in max_breaks is reached
 while number_of_breaks < max_breaks:
     time.sleep(intervall)
-    webbrowser.open(url)
+    webbrowser.open_new_tab(url)
     number_of_breaks += 1
